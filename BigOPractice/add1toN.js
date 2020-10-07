732030c1c4af3d499dc79c7236c8f63f49fb5832
Jeff-Adler <Jeffm.adler@gmail.com>
2020-10-07 11:40:21 -0400

    added performance.now checks

--- BigOPractice/add1toN.js
+++ BigOPractice/add1toN.js
@@ -1,3 +1,5 @@
+const { performance } = require("perf_hooks");
+
 const addUpTo = (n) => {
   let total = 0;
   for (let i = 1; i <= n; i++) {
@@ -10,5 +12,16 @@ const addUpTo2 = (n) => {
   return (n * (n + 1)) / 2;
 };
 
+let t1 = performance.now();
+addUpTo(100000000000);
+let t2 = performance.now();
+console.log(`Time to run addUpTo: ${t2 - t1 / 1000} seconds.`);
+
+t1 = performance.now();
+addUpTo2(100000000000);
+t2 = performance.now();
+console.log(`Time to run addUpTo: ${t2 - t1 / 1000} seconds.`);
+
 console.log("1: ", addUpTo(100));
+
 console.log("2: ", addUpTo2(100));
