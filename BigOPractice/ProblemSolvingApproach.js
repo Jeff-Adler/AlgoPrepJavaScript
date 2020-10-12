1cd55e8eb7be9c8d418a2a9a20ac8ccbda583653
Jeff-Adler <Jeffm.adler@gmail.com>
2020-10-12 19:06:52 -0400

    added solve/simplify

--- BigOPractice/ProblemSolvingApproach.js
+++ BigOPractice/ProblemSolvingApproach.js
@@ -6,9 +6,15 @@
     //How should I label the important pieces of data that are a part of the problem?
 
 //Explore Concrete Examples
+    //Simple examples
+    //Complex examples
+    //Examples with null or empty inputs
+    //Examples with invalid inputs
 
 //Break It Down
+    //Explicitly write out the steps you need to take
 
 //Solve/Simplify
+    //If you know you can't solve the entire problem, solve a simplified version of the problem first
 
 //Look back and Refactor
