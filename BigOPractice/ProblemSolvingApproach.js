26b4cb3494cd56e9e016d8760d8855667ab55222
Jeff-Adler <Jeffm.adler@gmail.com>
2020-10-12 18:44:37 -0400

    understand the problem

---
+++ BigOPractice/ProblemSolvingApproach.js
@@ -0,0 +1,14 @@
+//Understand the Problem
+    //Can I restate the problem in my own words?
+    //What are the inputs that go into the problem?
+    //What are the outputs that should come from the solution to the problem?
+    //Can the outputs be determined from the inputs? (In other words, do I have info to solve the problem?)
+    //How should I label the important pieces of data that are a part of the problem?
+
+//Explore Concrete Examples
+
+//Break It Down
+
+//Solve/Simplify
+
+//Look back and Refactor
