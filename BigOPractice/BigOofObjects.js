5c24c8aca124b8c7a9485debdc52265d3af60eb7
Jeff-Adler <Jeffm.adler@gmail.com>
2020-10-12 13:26:04 -0400

    addedBigOofObjects

---
+++ BigOPractice/BigOofObjects.js
@@ -0,0 +1,13 @@
+//BigOofObjects
+    //Insertion: O(1)
+    //Removal: O(1)
+    //Searching (for values): O(n)
+    //Accessing (keys): O(1)
+//Objects are good!
+//There is no insertion into a beginning or end -- its unordered!
+
+//Methods:
+    //Object.keys: O(n)
+    //Object.values: O(n)
+    //Object.entries: O(n)
+    //Object.hasOwnProperty: O(1) //return T or F is instructor has a key with that property
--- BigOPractice/LogarithmRulesOfThumb.js
+++ BigOPractice/LogarithmRulesOfThumb.js
@@ -1,5 +1,5 @@
 //Log of a number roughly measures to # of times you can divide that number by 2, before you get a value that's less than or equal to one
 
-//Certain searc algos have log time
+//Certain search algos have log time
 //Efficient sorting algos involves log time
 //recursion can involve log space complexity
