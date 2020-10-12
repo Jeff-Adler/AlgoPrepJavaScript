e249dcc1e2d4cd8f04f8723e10b0638dfe6871aa
Jeff-Adler <Jeffm.adler@gmail.com>
2020-10-12 18:16:07 -0400

    added bigOfof arrays

---
+++ BigOPractice/BigOofArrays.js
@@ -0,0 +1,10 @@
+//Use arrays:
+    //when you need order
+    //Insertion
+        //Pushing to end (push): O(1)
+        //Pushing to beginning (unshift): O(n)
+        //Remove from end (pop): O(1)
+        //Removing from beginning (shift): O(n)
+    //Removal
+    //Searching: O(n)
+    //Access: O(1)
