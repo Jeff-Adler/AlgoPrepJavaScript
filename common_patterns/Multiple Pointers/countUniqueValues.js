00bdb8a94e9849c23d8914622c250715673d7ff9
Jeff-Adler <Jeffm.adler@gmail.com>
2020-10-13 16:24:23 -0400

    added second solution to countUniqueValues

--- common_patterns/Multiple Pointers/countUniqueValues.js
+++ common_patterns/Multiple Pointers/countUniqueValues.js
@@ -29,3 +29,28 @@ console.log("Should return 5: ", countUniqueValues(arr1))
 console.log("Should return 1: ", countUniqueValues(arr2))
 console.log("Should return 6: ", countUniqueValues(arr3))
 console.log("Should return 0: ", countUniqueValues(arr4))
+
+//Udemy solution
+const countUniqueValues2 = (arr) => {
+    if (arr.length === 0) {
+        return 0
+    }
+    let i = 0
+    for (let j = 1; j < arr.length; j++) {
+        if (arr[i] !== arr[j]) {
+            i++
+            arr[i] = arr[j]
+        }
+    }
+    return i + 1
+} 
+
+const arr5 = [0,1,2,2,3,3,3,4]
+const arr6 = [0,0,0,0]
+const arr7 = [0,2,4,9,20,20,47]
+const arr8 = []
+
+console.log("Should return 5: ", countUniqueValues2(arr5))
+console.log("Should return 1: ", countUniqueValues2(arr6))
+console.log("Should return 6: ", countUniqueValues2(arr7))
+console.log("Should return 0: ", countUniqueValues2(arr8))
