56a77b0901a2e236a2690982e538b0b33fb06593
Jeff-Adler <Jeffm.adler@gmail.com>
2020-10-13 11:36:53 -0400

    attempting same_squared

---
+++ common_patterns/frequency_counters/frequency_counters.js
@@ -0,0 +1,4 @@
+//To collect frequency of values: use objects
+    //used when comparing inputs (e.g. anagram counters)
+    //usually O(n) time, as opposed to nested loops with O(n^2) time
+
---
+++ common_patterns/frequency_counters/sameSquared.js
@@ -0,0 +1,7 @@
+//Same: given two arrays, check if, given two arrays, every value in the array 
+// has its corresponding value SQUARED in the second array. 
+//Frequency of values must be the same. Order does not matter.
+
+const sameSquared = (arr1,arr2) => {
+    let 
+}
