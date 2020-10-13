af035d3d8f983a558139f1ce3425bf32e83e6652
Jeff-Adler <Jeffm.adler@gmail.com>
2020-10-13 18:44:50 -0400

    optimized maxSubarraySum

--- common_patterns/Sliding Window/maxSubarraySum.js
+++ common_patterns/Sliding Window/maxSubarraySum.js
@@ -1,7 +1,8 @@
 //MaxSubarraySum: function which accepts an (not necessarily sorted) array of integers and a number called n.
 //Function should calculate the maximum sum of n consecutive elements in the array.
 
-//naive solution
+//naive solution: this will not at all scale if the array and n get big enough. 
+//It will basically be quadratic time.
 const maxSubarraySum = (arr, n) => {
     if (n > arr.length) {
         return null
@@ -23,3 +24,21 @@ const maxSubarraySum = (arr, n) => {
 arr1 = [0,2,3,6,7,8,3,4]
 
 console.log(maxSubarraySum(arr1,3))
+
+//O(N) solution
+const maxSubarraySum = (arr,num) => {
+    let maxSum = 0
+    let tempSum = 0
+    if (arr.length < num) {
+        return null
+    }
+    for (let i = 0 ; i < num; i++) {
+        maxSum += arr[i]
+    }
+    tempSum = maxSum
+    for (let i = num; i < arr.length ; i++) {
+        tempSum = tempSum - arr[i - num] + arr[i]
+        maxSUm = Math.max(maxSum, tempSum)
+    }
+    return maxSum
+}
