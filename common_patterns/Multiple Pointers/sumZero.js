416e4687216aa341216bff5d87869300e825991c
Jeff-Adler <Jeffm.adler@gmail.com>
2020-10-13 14:46:18 -0400

    sumZero

---
+++ common_patterns/Multiple Pointers/sumZero.js
@@ -0,0 +1,3 @@
+//Write function which accepts sorted array of integers
+//The function should find the first pair where sum is ) (e.g. -3,3)
+//Return array that includes both values that sum to zero, or undefined if pair does not exist
