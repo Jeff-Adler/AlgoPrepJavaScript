084aead2d9e35b0be1d18f716bbb048d9a66461d
Jeff-Adler <Jeffm.adler@gmail.com>
2020-10-14 10:54:59 -0400

    added O(n) min subarraylens

---
+++ common_patterns/Sliding Window/findLongestSubstring.js
@@ -0,0 +1,14 @@
+// findLongestSubstring: accepts a string and returns the length of the Longest
+// Substring with all distinct characters.
+
+const findLongestSubstring = (str) => {
+
+}
+
+console.log("Should print 0:", findLongestSubstring(""))
+console.log("Should print 7:", findLongestSubstring("rithmschool"))
+console.log("Should print 6:", findLongestSubstring("thisisawesome"))
+console.log("Should print 7:", findLongestSubstring("thecatinthehat"))
+console.log("Should print 1:", findLongestSubstring("bbbbbb"))
+console.log("Should print 8:", findLongestSubstring("longestsubstring"))
+console.log("Should print 6:", findLongestSubstring("thisishowwedoit"))
--- common_patterns/Sliding Window/minSubarrayLength.js
+++ common_patterns/Sliding Window/minSubarrayLength.js
@@ -26,3 +26,33 @@ console.log("Should return 3: ", minSubArrayLength([1,4,16,22,5,7,8,9,10],39))
 console.log("Should return 5: ", minSubArrayLength([1,4,16,22,5,7,8,9,10],55))
 console.log("Should return 2: ", minSubArrayLength([4,3,3,8,1,2,3],11))
 console.log("Should return 0: ", minSubArrayLength([1,4,16,22,5,7,8,9,10],95))
+
+//O(n) solution
+function minSubArrayLen(nums, sum) {
+    let total = 0;
+    let start = 0;
+    let end = 0;
+    let minLen = Infinity;
+   
+    while (start < nums.length) {
+      // if current window doesn't add up to the given sum then 
+          // move the window to right
+      if(total < sum && end < nums.length){
+        total += nums[end];
+              end++;
+      }
+      // if current window adds up to at least the sum given then
+          // we can shrink the window 
+      else if(total >= sum){
+        minLen = Math.min(minLen, end-start);
+              total -= nums[start];
+              start++;
+      } 
+      // current total less than required total but we reach the end, need this or else we'll be in an infinite loop 
+      else {
+        break;
+      }
+    }
+   
+    return minLen === Infinity ? 0 : minLen;
+  }
