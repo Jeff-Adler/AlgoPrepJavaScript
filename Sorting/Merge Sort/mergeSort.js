a0f00031f477c6c3821568c01ff9dd90b6a99b9d
Jeff-Adler <Jeffm.adler@gmail.com>
2020-10-17 11:16:19 -0400

    added Colt's solution

--- Sorting/Merge Sort/mergeSort.js
+++ Sorting/Merge Sort/mergeSort.js
@@ -37,3 +37,31 @@ console.log("Should return [1,2,3,4,5,6]: ", merge([1,2,3],[4,5,6]))
 console.log("Should return [1,2,3,4,5,6]: ", merge([2,3,5],[1,4,6]))
 console.log("Should return [1,2,3,4,5,6]: ", merge([1,5,6],[2,3,4]))
 console.log("Should return [1,1,3,4,5,6]: ", merge([1,5,6],[1,3,4]))
+console.log("Should return [1,1,3,4,5]: ", merge([1,5],[1,3,4]))
+console.log("Should return [1,1,3,5,6]: ", merge([1,5,6],[1,3]))
+console.log("Should return [1,2,3,5,6,100,200]: ", merge([100,200], [1,2,3,5,6]))
+
+//Colt Solution:
+function merge2(arr1, arr2){
+    let results = [];
+    let i = 0;
+    let j = 0;
+    while(i < arr1.length && j < arr2.length){
+        if(arr2[j] > arr1[i]){
+            results.push(arr1[i]);
+            i++;
+        } else {
+            results.push(arr2[j])
+            j++;
+        }
+    }
+    while(i < arr1.length) {
+        results.push(arr1[i])
+        i++;
+    }
+    while(j < arr2.length) {
+        results.push(arr2[j])
+        j++;
+    }
+    return results;
+}
