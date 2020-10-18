75ca812889d943e86065bc7aeb417247211d8103
Jeff-Adler <Jeffm.adler@gmail.com>
2020-10-18 11:23:59 -0400

    implemented digitCount

--- Sorting/Radix Sort/radixSort.js
+++ Sorting/Radix Sort/radixSort.js
@@ -1,14 +1,30 @@
-const getDigit = (num,place) => {
-    const power = 10 ** place
 
-    const numByPower = Math.floor(Math.abs(num) / power)
+//returns value of some digit of a number
+const getDigit = (num,place) => {
+    // const power = 10 ** place
+
+    //Abs prevents negative numbers from interfering
+    //Math.floor just gets rid of any decimals
+    // const numByPower = Math.floor(Math.abs(num) / power)
     
-    return numByPower % 10
+    // return numByPower % 10
+
+    return (Math.floor(Math.abs(num) / (10 ** place))) % 10
+}
+// console.log("Should return 5: ", getDigit(12345,0))
+// console.log("Should return 4: ", getDigit(12345,1))
+// console.log("Should return 3: ", getDigit(12345,2))
+// console.log("Should return 2: ", getDigit(12345,3))
+// console.log("Should return 1: ", getDigit(12345,4))
+// console.log("Should return 0: ", getDigit(12345,5))
+
+//counts number of digits
+const digitCount = (num, counter = 0) => {
+    if (num < 1) return counter
+    return digitCount(num/10,++counter)
 }
 
-console.log("Should return 5: ", getDigit(12345,0))
-console.log("Should return 4: ", getDigit(12345,1))
-console.log("Should return 3: ", getDigit(12345,2))
-console.log("Should return 2: ", getDigit(12345,3))
-console.log("Should return 1: ", getDigit(12345,4))
-console.log("Should return 0: ", getDigit(12345,5))
+console.log("Should return 1: ", digitCount(1))
+console.log("Should return 2: ", digitCount(43))
+console.log("Should return 3: ", digitCount(847))
+console.log("Should return 4: ", digitCount(5847))
