307434e459d04823b728363ec1a61d2c9c9bdc83
Jeff-Adler <Jeffm.adler@gmail.com>
2020-10-19 09:00:18 -0400

    added alternative ways of solving parking system problem

--- Leetcode Problems/DesignParkingSystem.js
+++ Leetcode Problems/DesignParkingSystem.js
@@ -11,14 +11,54 @@ var ParkingSystem = function(big, medium, small) {
  * @param {number} carType
  * @return {boolean}
  */
+
 ParkingSystem.prototype.addCar = function(carType) {
-    if (this.lot[carType - 1] > 0) {
-        this.lot[carType - 1]--
-        return true
-    } else {
-        return false
-    }
+    if (this.lot[carType - 1] === 0) return false;
+    this.lot[carType - 1]--;
+    return true;
 };
+
+
+//class based inheritance
+// class ParkingSystem {
+//     constructor(big, medium, small) {
+//         this.capacities = {
+//             1:big,
+//             2:medium,
+//             3:small,
+//         }
+//     }
+
+//     addCar(type) {
+//         if(this.capacities[type] === 0)
+//             return false;
+        
+//         this.capacities[type]--;
+//         return true;
+        
+//     }
+// }
+
+// //prototypal inheritance with object
+// var ParkingSystem = function(big, medium, small) {
+//     this.lot = {
+//             1:big,
+//             2:medium,
+//             3:small,
+//         }
+// };
+
+// /** 
+//  * @param {number} carType
+//  * @return {boolean}
+//  */
+// ParkingSystem.prototype.addCar = function(carType) {
+//     if (this.lot[carType] === 0) return false;
+//     this.lot[carType]--;
+//     return true;
+// };
+
+
 
 /** 
  * Your ParkingSystem object will be instantiated and called as such:
