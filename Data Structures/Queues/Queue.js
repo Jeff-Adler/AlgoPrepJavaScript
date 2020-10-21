9838d929c2c33d185d6a70ba30fec477ef98c7d1
Jeff-Adler <Jeffm.adler@gmail.com>
2020-10-21 11:05:51 -0400

    enqueue/dequeue

---
+++ Data Structures/Queues/Queue.js
@@ -0,0 +1,50 @@
+class Node {
+    constructor (val) {
+        this.val = val
+        this.next = null
+    }
+}
+
+//We will add to the end (tail), and remove from beginning.
+class Queue {
+    constructor () {
+        this.first = null
+        this.last = null
+        this.size = 0
+    }
+
+    enqueue(val){
+        const newNode = new Node (val)
+        if (this.size === 0) {
+            this.first = newNode
+            this.last = newNode
+        } else {
+            this.last.next = newNode
+            this.last = newNode
+        }
+        return ++this.size
+    }
+
+    dequeue(){
+        if (this.size === 0) return null
+        const removedNode = this.first
+        if (this.size === 1) {
+            this.first = null
+            this.last = null
+        } else {
+            this.first = this.first.next
+        }
+        this.size--
+        removedNode.next = null
+        return removedNode.val
+    }
+}
+
+const queue = new Queue 
+queue.enqueue(1)
+queue.enqueue(2)
+queue.enqueue(3)
+console.log(queue)
+console.log(queue.dequeue())
+console.log(queue.dequeue())
+console.log(queue.dequeue())
---
+++ Data Structures/Queues/Queues.md
@@ -0,0 +1,17 @@
+Queues:
+
+FIFO: First in, First Out
+
+Use Cases:
+
+- background tasks
+- uploading resources
+- Printing/Task processing
+
+Implementations:
+
+- Array
+  - Push+Shift, Unshift+Pop
+    - You have no way around having to use the inefficient shift or unshift methods
+- Queue Class
+  - Use
