951c801acf85a6ab27d3b878f722353319760c20
Jeff-Adler <Jeffm.adler@gmail.com>
2020-10-27 09:07:57 -0400

    remove edge

--- Data Structures/Graphs/AdjacencyListGraph.js
+++ Data Structures/Graphs/AdjacencyListGraph.js
@@ -16,6 +16,10 @@ class Graph {
         this.adjacencyList[vertex1].push(vertex2)
         this.adjacencyList[vertex2].push(vertex1)
     }
+    removeEdge(vertex1,vertex2) {
+        this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter((vertex) => vertex !== vertex2 )
+        this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter((vertex) => vertex !== vertex1 )
+    }
 }
 
 const graph = new Graph()
@@ -25,4 +29,5 @@ graph.addVertex("Yokohama")
 graph.addVertex("Osaka")
 graph.addEdge("Tokyo","Yokohama")
 graph.addEdge("Tokyo","Osaka")
+graph.removeEdge("Tokyo","Yokohama")
 console.log(graph.getAdjacencyList())
