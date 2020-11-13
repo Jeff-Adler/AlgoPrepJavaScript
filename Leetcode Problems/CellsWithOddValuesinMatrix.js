adc78edf40c699af1cfa232a4ca8b1a44225385a
Jeff-Adler <Jeffm.adler@gmail.com>
2020-11-13 10:19:15 -0500

    fixing method

--- Leetcode Problems/CellsWithOddValuesinMatrix.js
+++ Leetcode Problems/CellsWithOddValuesinMatrix.js
@@ -5,41 +5,21 @@
  * @return {number}
  */
 var oddCells = function(n, m, indices) {
-    var arr = []
-    var subArr = []
-    for (let l = 0 ; l < n ; l++){
-        for (let j = 0 ; j < m ; j++) {
-            subArr.push(0)
-        }
-        arr.push(subArr)
-        subArr=[]
-    }
-    console.log(arr)
     
-//     var arr = [...Array(n)].fill([...Array(m)].fill(0))
-    
-    var row
-    var subArr2
-//     var subArr
-    
-    for (let i = 0 ; i < indices.length ; i++) {
-        row = parseInt(indices[i][0])
-        
+    var arr = [...Array(n)].fill([...Array(m)].fill(0))
+
+    for (let l = 0 ; l < indices.length ; l++) {
         //increment row
-        console.log(arr[row])
-        console.log(Array.isArray(arr[row]))
-        subArr2 = [...arr[row]]
-        subArr = arr[row].slice()
-        // subArr2 = [...arr[row]]
-        console.log(subArr)
-        console.log(subArr.map(x => x * 2))
-        
-        arr[indices[i][0]] = arr[indices[i][0]].map((val) => {return ++val})
+        for (let i = 0 ; i < arr[0].length ; i++) {
+            arr[indices[l][0]][i]++
+        }
+
         //increment column
-        arr = arr.map((row) => {return ++row[indices[i][1]]})
+        for (let j = 0 ; j < arr.length ; j++) {
+            arr[j][indices[l][1]]++
+        }
     }
-    console.log(arr)
-    return arr.length
+    return arr
 };
 
 console.log(oddCells(2,3,[[0,1],[1,1]]))
