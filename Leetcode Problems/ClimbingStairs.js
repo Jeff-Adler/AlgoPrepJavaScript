cde7b751cba5c74b8ecaf9b0a0fd630928327c8d
Jeff-Adler <Jeffm.adler@gmail.com>
2021-01-11 16:50:54 -0500

    climbing stairs

---
+++ Leetcode Problems/ClimbingStairs.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    let fib = [1,1]
+    for (let i = 2; i <= n; i++) {
+        fib[i] = fib[i-1] + fib[i-2];
+    }
+    return fib[n];
+};
