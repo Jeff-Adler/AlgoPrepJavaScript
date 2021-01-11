4fd239216d607ae25bbea130420399ff7f428e08
Jeff-Adler <Jeffm.adler@gmail.com>
2021-01-11 15:33:13 -0500

    reverseString optimized

--- Leetcode Problems/reverseString.js
+++ Leetcode Problems/reverseString.js
@@ -3,15 +3,22 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-    let start = 0
-    let end = s.length - 1 
-    let temp = ""
-    while (start < end) {
-        temp = s[start]
-        s[start] = s[end]
-        s[end] = temp
-        start++
-        end--
+    // let start = 0
+    // let end = s.length - 1 
+    // let temp = ""
+    // while (start < end) {
+    //     temp = s[start]
+    //     s[start] = s[end]
+    //     s[end] = temp
+    //     start++
+    //     end--
+    // }
+    // return s
+    
+    for (let i = 0 ; i < Math.floor(s.length/2) ; i++) {
+        temp = s[i]
+        s[i] = s[s.length - 1 - i]
+        s[s.length - 1 - i] = temp
     }
     return s
 };
