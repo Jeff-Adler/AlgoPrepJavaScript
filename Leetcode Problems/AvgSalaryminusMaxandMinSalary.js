0259d6ac76179c204fa528f780111e22445f73fc
Jeff-Adler <Jeffm.adler@gmail.com>
2021-01-11 14:29:59 -0500

    alt method avg salary excluding min or max

--- Leetcode Problems/AvgSalaryminusMaxandMinSalary.js
+++ Leetcode Problems/AvgSalaryminusMaxandMinSalary.js
@@ -3,5 +3,8 @@
  * @return {number}
  */
 var average = function(salary) {
-    return ((salary.reduce((ele, total) => ele + total)) - Math.max(...salary) - Math.min(...salary)) / (salary.length - 2)
+    // return ((salary.reduce((ele, total) => ele + total)) - Math.max(...salary) - Math.min(...salary)) / (salary.length - 2)
+    
+    salary.sort((a,b) => a - b)
+    return ((salary.reduce((ele, total) => ele + total) - salary[0] - salary[salary.length - 1]) / (salary.length - 2))
 };
